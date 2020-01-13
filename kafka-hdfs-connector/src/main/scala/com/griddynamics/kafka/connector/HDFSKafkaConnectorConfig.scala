623f5c9ac59f467c906f79748449f0eeb5f030ca
tkma1bp <dmitry.ershov@kohls.com>
2020-01-13 17:16:15 +0300

    Add HDFSKafkaConnectorConfig.scala

---
+++ kafka-hdfs-connector/src/main/scala/com/griddynamics/kafka/connector/HDFSKafkaConnectorConfig.scala
@@ -0,0 +1,40 @@
+package com.griddynamics.kafka.connector
+
+import org.apache.kafka.common.config.ConfigDef.{Importance, Type}
+import org.apache.kafka.common.config.{AbstractConfig, ConfigDef}
+
+import scala.collection.JavaConverters._
+
+object HDFSKafkaConnectorConfig {
+  private val TOPIC_CONFIG = "kafka.connect.topic"
+  private val TOPIC_DOC = "Topic to write to"
+  private val BATCH_SIZE_CONFIG = "kafka.connect.batch.size"
+  private val BATCH_SIZE_DOC = "Number of data points to retrieve at a time. Defaults to 100 (max value)"
+
+  def apply(parsedConfig: Map[String, String]): HDFSKafkaConnectorConfig = HDFSKafkaConnectorConfig(defaultConf(), parsedConfig)
+
+  def defaultConf(): ConfigDef = {
+    new ConfigDef()
+      .define(TOPIC_CONFIG, Type.STRING, Importance.HIGH, TOPIC_DOC)
+      .define(BATCH_SIZE_CONFIG, Type.INT, 100, new BatchSizeValidator(), Importance.LOW, BATCH_SIZE_DOC)
+  }
+}
+
+case class HDFSKafkaConnectorConfig(config: ConfigDef,
+                                    parsedConfig: Map[String, String]) extends AbstractConfig(config, parsedConfig.asJava) {
+
+  import com.griddynamics.kafka.connector.HDFSKafkaConnectorConfig._
+
+  def getTopic: String = this.getString(TOPIC_CONFIG)
+
+  def getBatchSIze: String = this.getString(BATCH_SIZE_CONFIG)
+
+}
+
+class BatchSizeValidator extends ConfigDef.Validator {
+  override def ensureValid(name: String, value: Any): Unit = {
+    val batchSize = value.asInstanceOf[Integer]
+    if (!(1 <= batchSize && batchSize <= 100))
+      throw new Exception("Batch Size must be a positive integer that's less or equal to 100")
+  }
+}
